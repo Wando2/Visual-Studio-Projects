6c16c7e6b719f44f55a8c749f729948b26e9271f
Wando2 <113200379+Wando2@users.noreply.github.com>
2023-11-12 20:00:33 -0300

    Update GroupBy.sql

--- Tutorial SQL Server/GroupBy.sql
+++ Tutorial SQL Server/GroupBy.sql
@@ -1,12 +1,15 @@
-USE [Cursos]
 
-SELECT TOP 10
-[Categoria].[Name] AS [CategoriaName],
-COUNT([Curso].[Id]) AS [CursosTotal]
-FROM
-[Categoria]
-INNER JOIN 
-[Curso] ON [Curso].[CategoriaId] = [Categoria].[Id]
 
-GROUP BY
-[Categoria].[Name]
+
+CREATE OR ALTER VIEW vwCursosPorCategoria AS 
+    SELECT TOP 10
+        [Categoria].[Name] AS [CategoriaName],
+        COUNT([Curso].[Id]) AS [CursosTotal]
+    FROM
+        [Categoria]
+    INNER JOIN 
+        [Curso] ON [Curso].[CategoriaId] = [Categoria].[Id]
+    GROUP BY
+        [Categoria].[Name]
+    HAVING
+        COUNT([Curso].[Id]) > 1;
