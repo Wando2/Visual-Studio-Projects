680beb2ed00f03ef1a80a66eafe6e11f159f95be
Wando2 <113200379+Wando2@users.noreply.github.com>
2023-12-03 17:57:30 -0300

    data

---
+++ DataAcess/DataAcess/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+class Program {
+    static void Main(string[] args) {
+       const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$";
+
+       var category = new Category();
+    }
+}
