aeee123200bb629c91e9db4668f1e1c4fec3bb8d
Wando2 <113200379+Wando2@users.noreply.github.com>
2023-12-25 14:59:49 -0300

    whatever

---
+++ Blog2/Blog2/Models/Role.cs
@@ -0,0 +1,16 @@
+namespace Blog2.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("[Role]")]
+
+
+    public class Role
+    {
+        [Key]  public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Slug { get; set; }
+    }
+
+}
