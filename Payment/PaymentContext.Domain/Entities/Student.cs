f5464fd047ec2ef32d905d0ae5d9bbebfdef8e48
Wando2 <113200379+Wando2@users.noreply.github.com>
2024-01-16 15:22:48 -0300

    ddd

---
+++ Payment/PaymentContext.Domain/Entities/Student.cs
@@ -0,0 +1,17 @@
+﻿using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Domain;
+
+public class student
+{
+    public string FisrtName { get; set; }
+    public string LastName { get; set; }
+    public string Document { get; set; }
+    public string Email { get; set; }
+
+    public string Address { get; set; }
+
+    public List<Subscription> Subscriptions { get; set; }
+
+    
+}
